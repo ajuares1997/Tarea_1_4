e3e6a99b235364f8ff3ef6767c74dd427d3a77f8
ajuares1997 <luisj4539@gmail.com>
2023-02-23 16:23:29 -0600

    Luis Romero

--- Tarea_1_4/App.xaml.cs
+++ Tarea_1_4/App.xaml.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
+using Tarea_1_4.Controllers;
+using Tarea_1_4.Views;
 
 namespace Tarea_1_4
 {
     public partial class App : Application
     {
+        static EmpleadosDB basedatos;
+
+        public static EmpleadosDB BaseDatos
+        {
+            get
+            {
+                if (basedatos == null)
+                {
+                    basedatos = new EmpleadosDB(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EmpleadosDB.db3"));
+                }
+                return basedatos;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ListarEmpleados());
         }
 
         protected override void OnStart()
---
+++ Tarea_1_4/Views/ListarEmpleados.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea_1_4.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Tarea_1_4;
+
+namespace Tarea_1_4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListarEmpleados : ContentPage
+    {
+        public ListarEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private async void toolmenu_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listapersonas.ItemsSource = await App.BaseDatos.obtenerListaEmple();
+        }
+
+        private async void liestapersonas_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var persona = (Empleados)e.Item;
+            VerImagen page = new VerImagen();
+            page.BindingContext = persona;
+            await Navigation.PushAsync(page);
+
+        }
+    }
+}
